0b2bfd667adaf67784be82f4e1418c3f3d517051
LaughingLeader <laughingleader@gmail.com>
2020-01-03 15:30:02 -0600

    Added Pet Pal mod support, reworked icon overriding

--- Mods/ZZZ_GreenNecroFire_0bc91e73-ce14-4d3f-934c-3024a8ba348d/Story/RawFiles/Lua/Bootstrap.lua
+++ Mods/ZZZ_GreenNecroFire_0bc91e73-ce14-4d3f-934c-3024a8ba348d/Story/RawFiles/Lua/Bootstrap.lua
@@ -18,12 +18,16 @@ local stat_overrides = {
     Projectile_EnemyFireball_Cursed = {
         Template = "58827195-8768-4711-9892-58144d11aa16",
         PrepareEffect = "LLGREENFLAME_FX_Skills_NecroFireball_Prepare_Fire_Root_01,KeepRot;LLGREENFLAME_FX_Skills_NecroFireball_Prepare_Fire_Hand_01:Dummy_L_HandFX,Dummy_R_HandFX",
-        CastEffect = "LLGREENFLAME_FX_Skills_NecroFireball_Cast_Fire_Hand_01:Dummy_R_HandFX:cast;LLGREENFLAME_FX_Skills_NecroFireball_Cast_Throw_Hand_01:Dummy_R_HandFX"
+        CastEffect = "LLGREENFLAME_FX_Skills_NecroFireball_Cast_Fire_Hand_01:Dummy_R_HandFX:cast;LLGREENFLAME_FX_Skills_NecroFireball_Cast_Throw_Hand_01:Dummy_R_HandFX",
+        DisplayName = "Projectile_LLGREENFLAME_CursedFireball_DisplayName",
+        Description = "Projectile_LLGREENFLAME_CursedFireball_Description"
     },
     Projectile_EnemyFireball_Cursed_Insect = {
         Template = "58827195-8768-4711-9892-58144d11aa16",
         PrepareEffect = "LLGREENFLAME_FXcharacter_Creatures_Giant_Insect_Fire_Prepare_01_Root,KeepRot;LLGREENFLAME_FXcharacter_Creatures_Giant_Insect_Fire_Prepare_01:Dummy_L_HandFX,Dummy_R_HandFX",
-        CastEffect = "LLGREENFLAME_FX_Skills_NecroFireball_Cast_Fire_Hand_01:Dummy_CastFX:cast;LLGREENFLAME_FX_Skills_NecroFireball_Cast_Throw_Hand_01:Dummy_CastFX"
+        CastEffect = "LLGREENFLAME_FX_Skills_NecroFireball_Cast_Fire_Hand_01:Dummy_CastFX:cast;LLGREENFLAME_FX_Skills_NecroFireball_Cast_Throw_Hand_01:Dummy_CastFX",
+        DisplayName = "Projectile_LLGREENFLAME_CursedFireball_DisplayName",
+        Description = "Projectile_LLGREENFLAME_CursedFireball_Description"
     },
     Target_NecrofireInfusion = {
         PrepareEffect = "LLGREENFLAME_FX_Skills_Totem_Prepare_Totem_Root_NecroFire_01,KeepRot,Detach",
@@ -66,19 +70,94 @@ stat_overrides["Projectile_EnemyInfectiousFlame"] = stat_overrides["Projectile_I
 stat_overrides["Projectile_IncarnateInfectiousFlame"] = stat_overrides["Projectile_InfectiousFlame"]
 stat_overrides["Projectile_EnemyInfectiousFlame_Puppet"] = stat_overrides["Projectile_InfectiousFlame"]
 
-stat_overrides["Projectile_IncarnateInfectiousFlame"] = stat_overrides["Target_NecrofireInfusion"]
+local icon_overrides = {
+    Projectile_InfectiousFlame = "LLGREENFLAME_Skills_EpidemicOfFire",
+    Target_NecrofireInfusion = "LLGREENFLAME_Skills_NecroFireinfusion",
+    Projectile_EnemyFireball_Cursed = "LLGREENFLAME_Skills_CursedFireball",
+    INF_NECROFIRE = "LLGREENFLAME_Skills_NecroFireinfusion",
+    INF_NECROFIRE_G = "LLGREENFLAME_Skills_NecroFireinfusion",
+    NECROFIRE = "LLGREENFLAME_Status_NecroFire",
+    PERMANENT_NECROFIRE = "LLGREENFLAME_Status_NecroFire"
+}
+
+local function get_stat_override(statname)
+    for k,v in pairs(stat_overrides) do
+        if k == statname then
+            return v
+        end
+    end
+    return nil
+end
 
 local ModuleLoad = function ()
     Ext.Print("[ZZZ_GreenNecrofire:Bootstrap.lua] Module is loading.")
-    if _G["LeaderLib"] ~= nil or Ext.IsModLoaded("LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c") then
-        Ext.Print("[LLGREENFLAME:Bootstrap.lua] Overriding green flame skills/statuses to use LeaderLib's icons.")
-        Ext.StatSetAttribute("Projectile_InfectiousFlame", "Icon", "LLGREENFLAME_Skills_EpidemicOfFire")
-        Ext.StatSetAttribute("Target_NecrofireInfusion", "Icon", "LLGREENFLAME_Skills_NecroFireinfusion")
-        Ext.StatSetAttribute("INF_NECROFIRE", "Icon", "LLGREENFLAME_Skills_NecroFireinfusion")
-        Ext.StatSetAttribute("INF_NECROFIRE_G", "Icon", "LLGREENFLAME_Skills_NecroFireinfusion")
-        Ext.StatSetAttribute("NECROFIRE", "Icon", "LLGREENFLAME_Status_NecroFire")
-        Ext.StatSetAttribute("PERMANENT_NECROFIRE", "Icon", "LLGREENFLAME_Status_NecroFire")
+
+    --Larian's Pet Power Mod
+    if Ext.IsModLoaded("d2507d43-efce-48b8-ba5e-5dd136c715a7") then
+        Ext.Print("[LLGREENFLAME:Bootstrap.lua] Pet Power Larian mod detected. Overriding relevant stats.")
+        -- stat_overrides["Projectile_Summon_BomberProjectile_Necrofire"] = {
+        --     Template = "c788b988-a772-47c7-ae4f-844a170435f3"
+        -- }
+        -- stat_overrides["Shout_Summon_SuicideBomberNecrofire"] = {
+        --     CastEffect = "LLGREENFLAME_FX_Skills_Fire_Cast_Shout_Supernova_01,Detach"
+        -- }
+        stat_overrides["Projectile_Summon_InfectiousFire_Shorter"] = {
+            Template = "086f73c3-b7c7-42c0-bdbe-6e5ff52e3f91",
+            PrepareEffect = "LLGREENFLAME_FX_Skills_Fire_Prepare_Voodoo_Root_02,KeepRot,Detach;LLGREENFLAME_FX_Skills_Fire_Prepare_Voodoo_Hand_01:Dummy_R_HandFX,Dummy_L_HandFX",
+            CastEffect = "LLGREENFLAME_FX_Skills_Fire_Cast_Beam_Fire_Hand_01:Dummy_CastFX:cast",
+            --CastEffect = "LLGREENFLAME_FX_Skills_Fire_Cast_Beam_Fire_Hand_01_Shorter:Dummy_CastFX:cast"
+        }
+
+        stat_overrides["INF_NECROFIRE_INCARNATE_S"] = stat_overrides["INF_NECROFIRE"]
+        stat_overrides["INF_NECROFIRE_INCARNATE_G"] = stat_overrides["INF_NECROFIRE_G"]
+
+        icon_overrides["Projectile_Summon_InfectiousFire_Shorter"] = icon_overrides["Projectile_InfectiousFlame"]
+        
+        stat_overrides["INF_NECROFIRE_INCARNATE_S"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_INCARNATE_G"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_BONEPILE"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_PLANT"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_CORPSE"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_OILBLOB"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_FIRESLUG"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_CONDOR"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_NEWT"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_POISONSLUG"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_SOULWOLF"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_TOY"]["FormatColor"] = "Poison"
+        stat_overrides["INF_NECROFIRE_CAT"]["FormatColor"] = "Poison"
+
+        icon_overrides["INF_NECROFIRE_INCARNATE_S"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_INCARNATE_G"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_BONEPILE"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_PLANT"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_CORPSE"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_OILBLOB"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_FIRESLUG"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_CONDOR"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_NEWT"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_POISONSLUG"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_SOULWOLF"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_TOY"] = icon_overrides["INF_NECROFIRE"]
+        icon_overrides["INF_NECROFIRE_CAT"] = icon_overrides["INF_NECROFIRE"]
     end
+
+    --LeaderLib_7e737d2f-31d2-4751-963f-be6ccc59cd0c
+    if _G["LeaderLib"] ~= nil or Ext.IsModLoaded("7e737d2f-31d2-4751-963f-be6ccc59cd0c") then
+        Ext.Print("[LLGREENFLAME:Bootstrap.lua] Overriding necrofire skills/statuses to use LeaderLib's icons.")
+        for stat,value in pairs(icon_overrides) do
+            --Ext.StatSetAttribute(stat, "Icon", value)
+            local stat_override_entry = get_stat_override(stat)
+            if stat_override_entry ~= nil then
+                stat_override_entry["Icon"] = value
+            else
+                stat_overrides[stat] = {
+                    Icon = value
+                }
+            end
+        end
+    end
+
     for statname,overrides in pairs(stat_overrides) do
         for property,value in pairs(overrides) do
             Ext.Print("[LLGREENFLAME:Bootstrap.lua] Overriding stat: " .. statname .. " (".. property ..") = \"".. value .."\"")
@@ -93,6 +172,7 @@ local ModuleLoad = function ()
     if Ext.IsModLoaded("OdinbladePyromancer_aab53301-4f38-1d49-91f7-28dfa468084b") then
         Ext.Print("[LLGREENFLAME:Bootstrap.lua] Overriding Projectile_InfectiousFlame_Description with 'Projectile_InfectiousFlame_LLGREENFLAME_Odin_Description'.")
         Ext.StatSetAttribute("Projectile_InfectiousFlame", "Description", "Projectile_InfectiousFlame_LLGREENFLAME_Odin_Description")
+        Ext.StatSetAttribute("Projectile_IncarnateFireball", "Description", "Projectile_LLGREENFLAME_CursedFireball_Odin_Description")
     elseif Ext.StatGetAttribute("Projectile_InfectiousFlame", "Description") == "Projectile_InfectiousFlame_Description" then
         Ext.Print("[LLGREENFLAME:Bootstrap.lua] Overriding Projectile_InfectiousFlame_Description with 'Projectile_InfectiousFlame_LLGREENFLAME_Description'.")
         Ext.StatSetAttribute("Projectile_InfectiousFlame", "Description", "Projectile_InfectiousFlame_LLGREENFLAME_Description")
